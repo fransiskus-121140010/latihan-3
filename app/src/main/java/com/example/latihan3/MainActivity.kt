70f775ebea365474097b53d915f615fc7f65393c
fransiskus-121140010 <fransxa78@gmail.com>
2024-03-14 20:25:58 +0700

    first commit

---
+++ app/src/main/java/com/example/latihan3/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.latihan3
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.latihan3.databinding.ActivityMainBinding
+import com.example.latihan3.ui.theme.Latihan3Theme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Menginisialisasi binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // Mengakses elemen UI melalui variabel binding
+        binding.textView.text = "Hello, View Binding!"
+        binding.button.setOnClickListener {
+            Log.d("MainActivity", "Button clicked")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Latihan3Theme {
+        Greeting("Android")
+    }
+}
